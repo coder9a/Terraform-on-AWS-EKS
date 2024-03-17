ca36591541bb17acb51ab7f1d9a9247f4081d597
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2024-03-17 15:57:13 +0000

    commented private node group

--- eks-outputs.tf
+++ eks-outputs.tf
@@ -66,7 +66,7 @@ output "node_group_public_version" {
 }
 
 # EKS Node Group Outputs - Private
-
+/*
 output "node_group_private_id" {
   description = "Node Group 1 ID"
   value       = aws_eks_node_group.eks_ng_private.id
@@ -86,3 +86,5 @@ output "node_group_private_version" {
   description = "Private Node Group Kubernetes Version"
   value       = aws_eks_node_group.eks_ng_private.version
 }
+
+*/
