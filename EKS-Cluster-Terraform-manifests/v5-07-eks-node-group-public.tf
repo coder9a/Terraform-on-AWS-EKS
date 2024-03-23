57f06958d7035c7a7c08027b2b763586c94dd73b
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2024-03-23 03:08:40 +0000

    Updated instance type

--- EKS-Cluster-Terraform-manifests/v5-07-eks-node-group-public.tf
+++ EKS-Cluster-Terraform-manifests/v5-07-eks-node-group-public.tf
@@ -10,7 +10,7 @@ resource "aws_eks_node_group" "eks_ng_public" {
   ami_type       = "AL2_x86_64"
   capacity_type  = "ON_DEMAND"
   disk_size      = 20
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.small"]
 
 
   remote_access {
