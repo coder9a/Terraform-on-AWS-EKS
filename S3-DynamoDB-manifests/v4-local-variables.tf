12545dd516373b133aaef88c7192846e775672ea
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2024-04-08 11:25:43 +0000

    added new s3 folder and dynamodb table

--- S3-DynamoDB-manifests/v4-local-variables.tf
+++ S3-DynamoDB-manifests/v4-local-variables.tf
@@ -2,10 +2,12 @@ locals {
   s3_folders = [
     "dev/eks-cluster",
     "dev/ebs-storage",
-    "dev/eks-irsa-demo"
+    "dev/eks-irsa-demo",
+    "dev/aws-lbc"
   ]
   dynamodb_tables = [
   "dev-ebs-storage",
-  "eks-dynamodb-lock-table"
+  "eks-dynamodb-lock-table",
+  "dev-aws-lbc"
   ]
 }
