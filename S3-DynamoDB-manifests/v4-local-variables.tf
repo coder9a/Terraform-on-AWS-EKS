5c30842ffcf8041d4e3229b609265c701763d639
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2024-04-14 12:02:02 +0000

    Added Ingress ALB files

--- S3-DynamoDB-manifests/v4-local-variables.tf
+++ S3-DynamoDB-manifests/v4-local-variables.tf
@@ -1,13 +1,14 @@
 locals {
   s3_folders = [
-    "dev/eks-cluster",
-    "dev/ebs-storage",
-    "dev/eks-irsa-demo",
-    "dev/aws-lbc"
+    # "dev/eks-cluster",
+    # "dev/ebs-storage",
+    # "dev/eks-irsa-demo",
+    # "dev/aws-lbc"
   ]
   dynamodb_tables = [
   "dev-ebs-storage",
   "eks-dynamodb-lock-table",
-  "dev-aws-lbc"
+  "dev-aws-lbc",
+  "dev-aws-lbc-ingress"
   ]
 }
